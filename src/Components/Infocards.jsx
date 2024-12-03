0707564f85606bfaee97af5cda0b0cb60ffb8367
Stan <stan.strutt@gmail.com>
2024-12-03 15:25:35 +0000

    Aligned text and added icons to resources

--- src/Components/Infocards.jsx
+++ src/Components/Infocards.jsx
@@ -1,15 +1,16 @@
 import "./Infocards.css"
-import { Fade } from 'react-awesome-reveal';
 
 
 export default function InfoCards(props) {
 
-    const safeValue = (value) => value || "";
+    const safeValue = (value) => value || ""
+
+    
 
     if (props.filteredResults.length === 0) return null;
 
+
     return(
-        <Fade cascade>
         <div className="cards"> 
         {props.filteredResults.map((resource) => (
             <div key={resource.id} className="card">
@@ -25,11 +26,21 @@ export default function InfoCards(props) {
                 <div className="card-resources">
                     <hr/>
                     <div className="resource-details">
-                        <p>{safeValue(resource.contact?.phoneNumber)}</p>
-                        <p>{safeValue(resource.contact?.email)}</p>                     
-                        <p>{safeValue(resource.contact?.website)}</p>                       
+                        <div className="phone" style={{ display: resource.contact?.phoneNumber ? 'flex' : 'none' }}>
+                            <img src="https://media.istockphoto.com/id/1494579261/vector/telephone-handset-simple-circle-icon.jpg?s=612x612&w=0&k=20&c=bpf-J3OuFFZDNS3gQQ25Xgp6PKJ_lhbmvuO-kYgNCSg=" width="20px" height="20px" alt=""/>
+                            <p>{safeValue(resource.contact?.phoneNumber)}</p>
+                        </div>
+                        <div className="email" style={{ display: resource.contact?.email ? 'flex' : 'none' }}>
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/At_sign.svg/800px-At_sign.svg.png" alt="" width="20px" height="20px"/>
+                            <p>{safeValue(resource.contact?.email)}</p>
+                        </div>
+                        <div className="website" style={{ display: resource.contact?.website ? 'flex' : 'none' }}>
+                            <img src="https://cdn.pixabay.com/photo/2016/11/30/17/10/web-1873373_1280.png" alt="" width="20px" height="20px"/>                 
+                            <p>{safeValue(resource.contact?.website)}</p>
+                        </div>          
                     </div>
-                    <div className="resource-address">
+                    <div className="resource-address" style={{ display: resource.contact.address?.line1 ? 'flex' : 'none' }}>
+                        <img src="https://www.iconpacks.net/icons/2/free-location-icon-2952-thumb.png" width="20px" height="20px" alt=""/>
                         <p>{safeValue(resource.contact.address?.line1)}</p>
                         <p>{safeValue(resource.contact.address?.line2)}</p>
                         <p>{safeValue(resource.contact.address?.city)}</p>
@@ -40,7 +51,6 @@ export default function InfoCards(props) {
             </div>
         ))}
     </div>
-    </Fade>
 )}
 
 
